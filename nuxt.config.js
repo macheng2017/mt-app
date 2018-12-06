17d86512d078a62026f2ef3ebf97292239ae066d
mac <mac@macs-iMac.local>
2018-12-06 10:46:44 +0800

    将首页分模块并创建相应的模块文件

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,8 +26,8 @@ module.exports = {
   */
   css: [
     'element-ui/lib/theme-chalk/reset.css',
-    'element-ui/lib/theme-chalk/index.css'
-    // '~assets/css/main.css'
+    'element-ui/lib/theme-chalk/index.css',
+    '@assets/css/main.css'
   ],
 
   /*
